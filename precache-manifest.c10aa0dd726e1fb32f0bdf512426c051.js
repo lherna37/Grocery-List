dd89c82ad49586882cdcdac31bf7c6ba029546cd
Rainy <rainy@Daniels-MacBook-Pro.local>
2020-09-26 12:42:14 -0700

    Updates

---
+++ precache-manifest.c10aa0dd726e1fb32f0bdf512426c051.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ce4ebf579487906bfa0924e96e6cc3",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "e7802c6d26a5d6b8f435",
+    "url": "/grocery-list/static/css/main.531d322a.chunk.css"
+  },
+  {
+    "revision": "4892a99d2c59e4743b59",
+    "url": "/grocery-list/static/js/2.dc302682.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/grocery-list/static/js/2.dc302682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7802c6d26a5d6b8f435",
+    "url": "/grocery-list/static/js/main.78d31f79.chunk.js"
+  },
+  {
+    "revision": "3f79d6f9011e342088c4",
+    "url": "/grocery-list/static/js/runtime-main.a83510f9.js"
+  },
+  {
+    "revision": "8a09af352882b39f5920d3c2dabf156b",
+    "url": "/grocery-list/static/media/circle-down.8a09af35.svg"
+  },
+  {
+    "revision": "30d2802f4a6e8c206d1587eb3e440767",
+    "url": "/grocery-list/static/media/circle-up.30d2802f.svg"
+  },
+  {
+    "revision": "810f1a2690775a471a4441bf7013ede8",
+    "url": "/grocery-list/static/media/more_vert.810f1a26.svg"
+  }
+]);
